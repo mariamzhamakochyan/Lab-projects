5df47ef968b7de84a2b78bfa1b6f3554c61b0b12
mariamzhamakochyan <mariamzhamakochyan@gmail.com>
2022-09-13 17:45:57 +0400

    student database system

--- admin.py
+++ admin.py
@@ -19,42 +19,42 @@ from PyQt5.QtWidgets import (QApplication, QHeaderView, QLabel, QLineEdit,
     QPushButton, QSizePolicy, QTableWidget, QTableWidgetItem,
     QWidget)
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(400, 500)
-        Form.setStyleSheet(u"")
-        self.lineEdit_id = QLineEdit(Form)
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(400, 500)
+        Dialog.setStyleSheet(u"")
+        self.lineEdit_id = QLineEdit(Dialog)
         self.lineEdit_id.setObjectName(u"lineEdit_id")
         self.lineEdit_id.setGeometry(QRect(10, 50, 211, 31))
-        self.lineEdit_first = QLineEdit(Form)
+        self.lineEdit_first = QLineEdit(Dialog)
         self.lineEdit_first.setObjectName(u"lineEdit_first")
         self.lineEdit_first.setGeometry(QRect(10, 90, 211, 31))
-        self.lineEdit_last = QLineEdit(Form)
+        self.lineEdit_last = QLineEdit(Dialog)
         self.lineEdit_last.setObjectName(u"lineEdit_last")
         self.lineEdit_last.setGeometry(QRect(10, 130, 211, 31))
-        self.pushButton_add = QPushButton(Form)
+        self.pushButton_add = QPushButton(Dialog)
         self.pushButton_add.setObjectName(u"pushButton_add")
         self.pushButton_add.setGeometry(QRect(280, 60, 101, 21))
         self.pushButton_add.setStyleSheet(u"background-color: rgb(61, 62, 255);")
-        self.lineEdit_email = QLineEdit(Form)
+        self.lineEdit_email = QLineEdit(Dialog)
         self.lineEdit_email.setObjectName(u"lineEdit_email")
         self.lineEdit_email.setGeometry(QRect(10, 170, 211, 31))
-        self.pushButton_modify = QPushButton(Form)
+        self.pushButton_modify = QPushButton(Dialog)
         self.pushButton_modify.setObjectName(u"pushButton_modify")
         self.pushButton_modify.setGeometry(QRect(280, 110, 101, 21))
         self.pushButton_modify.setStyleSheet(u"background-color: rgb(61, 62, 255);")
-        self.pushButton_delet = QPushButton(Form)
+        self.pushButton_delet = QPushButton(Dialog)
         self.pushButton_delet.setObjectName(u"pushButton_delet")
         self.pushButton_delet.setGeometry(QRect(280, 160, 101, 21))
         self.pushButton_delet.setStyleSheet(u"background-color: rgb(61, 62, 255);\n"
 "")
-        self.label = QLabel(Form)
+        self.label = QLabel(Dialog)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(120, 10, 181, 31))
         self.label.setStyleSheet(u"")
-        self.tableWidget = QTableWidget(Form)
+        self.tableWidget = QTableWidget(Dialog)
         if (self.tableWidget.columnCount() < 4):
             self.tableWidget.setColumnCount(4)
         __qtablewidgetitem = QTableWidgetItem()
@@ -67,36 +67,36 @@ class Ui_Form(object):
         self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
         self.tableWidget.setObjectName(u"tableWidget")
         self.tableWidget.setGeometry(QRect(15, 231, 371, 261))
-        self.pushButton_load = QPushButton(Form)
+        self.pushButton_load = QPushButton(Dialog)
         self.pushButton_load.setObjectName(u"pushButton_load")
         self.pushButton_load.setGeometry(QRect(290, 200, 81, 21))
         self.pushButton_load.setStyleSheet(u"background-color: rgb(61, 62, 255);\n"
 "")
 
-        self.retranslateUi(Form)
+        self.retranslateUi(Dialog)
 
-        QMetaObject.connectSlotsByName(Form)
+        QMetaObject.connectSlotsByName(Dialog)
     # setupUi
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.lineEdit_id.setPlaceholderText(QCoreApplication.translate("Form", u"  Enter student ID", None))
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Form", None))
+        self.lineEdit_id.setPlaceholderText(QCoreApplication.translate("Dialog", u"  Enter student ID", None))
         self.lineEdit_first.setText("")
-        self.lineEdit_first.setPlaceholderText(QCoreApplication.translate("Form", u"  Enter student First Name", None))
-        self.lineEdit_last.setPlaceholderText(QCoreApplication.translate("Form", u"  Enter student Last Name", None))
-        self.pushButton_add.setText(QCoreApplication.translate("Form", u"ADD", None))
-        self.lineEdit_email.setPlaceholderText(QCoreApplication.translate("Form", u"  Enter student Email", None))
-        self.pushButton_modify.setText(QCoreApplication.translate("Form", u"MODIFY", None))
-        self.pushButton_delet.setText(QCoreApplication.translate("Form", u"DELET", None))
-        self.label.setText(QCoreApplication.translate("Form", u"  STUDENTS  INFORMATION", None))
+        self.lineEdit_first.setPlaceholderText(QCoreApplication.translate("Dialog", u"  Enter student First Name", None))
+        self.lineEdit_last.setPlaceholderText(QCoreApplication.translate("Dialog", u"  Enter student Last Name", None))
+        self.pushButton_add.setText(QCoreApplication.translate("Dialog", u"ADD", None))
+        self.lineEdit_email.setPlaceholderText(QCoreApplication.translate("Dialog", u"  Enter student Email", None))
+        self.pushButton_modify.setText(QCoreApplication.translate("Dialog", u"MODIFY", None))
+        self.pushButton_delet.setText(QCoreApplication.translate("Dialog", u"DELET", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"  STUDENTS  INFORMATION", None))
         ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
-        ___qtablewidgetitem.setText(QCoreApplication.translate("Form", u"ID", None));
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Dialog", u"ID", None));
         ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
-        ___qtablewidgetitem1.setText(QCoreApplication.translate("Form", u"FIRST NAME", None));
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Dialog", u"FIRST NAME", None));
         ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
-        ___qtablewidgetitem2.setText(QCoreApplication.translate("Form", u"LAST NAME", None));
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Dialog", u"LAST NAME", None));
         ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
-        ___qtablewidgetitem3.setText(QCoreApplication.translate("Form", u"EMAIL", None));
-        self.pushButton_load.setText(QCoreApplication.translate("Form", u"LOAD", None))
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("Dialog", u"EMAIL", None));
+        self.pushButton_load.setText(QCoreApplication.translate("Dialog", u"LOAD", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -1,31 +1,56 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox
 from PyQt5.uic import loadUi
+
+
 
 class Login(QDialog):
     def __init__(self):
-        super(Login,self).__init__()
-        loadUi("login.ui",self)
-        self.b2.clicked.connect(self.loginfunction)
-        self.b3.clicked.connect(self.loginfunction)
+        super(Login, self).__init__()
+        loadUi("login.ui", self)
+        self.b2.clicked.connect(self.studentfunc)
+        self.b3.clicked.connect(self.adminfunc)
         self.b1.clicked.connect(self.gotocreate)
 
 
-    def loginfunction(self):
-        username=self.username.text()
-        password=self.password.text()
-        print("Successfully logged in with username: ", username, "and password:", password)
+    def studentfunc(self):
+        username = self.username.text()
+        password = self.password.text()
+
+        if username != "":
+            if password != "":
+                print("connect bdi enenq heto student i acci het")
+            else:
+                QMessageBox.about(self, "Warning", "Please Enter Password")
+        else:
+            QMessageBox.about(self, "Warning", "Please Enter username")
 
     def gotocreate(self):
-        createacc=CreateAcc()
+        createacc = CreateAcc()
         widget.addWidget(createacc)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
+
+
+    def adminfunc(self):
+        username = self.username.text()
+        password = self.password.text()
+        info = AdminAcc()
+        widget.addWidget(info)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+
+class AdminAcc(QDialog):
+    def __init__(self):
+        super(AdminAcc, self).__init__()
+        loadUi("admin.ui", self)
+
+
 class CreateAcc(QDialog):
     def __init__(self):
-        super(CreateAcc,self).__init__()
-        loadUi("register.ui",self)
+        super(CreateAcc, self).__init__()
+        loadUi("register.ui", self)
         self.stdreg.clicked.connect(self.createaccfunction)
         self.adreg.clicked.connect(self.createaccfunction)
 
@@ -33,18 +58,28 @@ class CreateAcc(QDialog):
 
     def createaccfunction(self):
         username = self.username.text()
-        if self.password.text()==self.confirmpassword.text():
-            password=self.password.text()
+        if self.password.text() == self.confirmpassword.text():
+            password = self.password.text()
+            confirmpassword = self.confirmpassword.text()
             print("Successfully created acc with username: ", username, "and password: ", password)
-            login=Login()
+            login = Login()
             widget.addWidget(login)
             widget.setCurrentIndex(widget.currentIndex()+1)
+            if username != "":
+                if password != "":
+                    if confirmpassword != "":
+                        QMessageBox.about(self, "Warning", "you successfully registered")
+                    else:
+                        QMessageBox.about(self, "Warning", "Please confirm the password")
+                else:
+                    QMessageBox.about(self, "Warning", "Please Enter your Password")
+            else:
+                QMessageBox.about(self, "Warning", "Please Enter your Username")
 
 
-
-app=QApplication(sys.argv)
-mainwindow=Login()
-widget=QtWidgets.QStackedWidget()
+app = QApplication(sys.argv)
+mainwindow = Login()
+widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
 widget.setFixedWidth(400)
 widget.setFixedHeight(500)
